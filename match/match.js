26b88707610b6c2b44c55c14f579de9d7a204267
Rob <r.p.ivie@gmail.com>
2017-09-09 10:36:58 -0700

    various enhancements
    card presentation
    adding delete funtion

--- match/match.js
+++ match/match.js
@@ -12,7 +12,7 @@ app.controller('tagsCtrl', ['$scope','$http', function($scope, $http) {
 	});
 
 	$scope.source = null;
-	$scope.exchange = function(element, tag) {
+	$scope.exchange = function(tag) {
 		if ($scope.source === null) {
 			tag.isExchanging = !tag.isExchanging;//change styles
 			$scope.source = tag;//store
@@ -27,6 +27,65 @@ app.controller('tagsCtrl', ['$scope','$http', function($scope, $http) {
 			//reset
 			$scope.source = null;
 		}
-		console.log(element);
+	}
+	$scope.unlink = function(parent, tag) {
+		function deleteLeft(parent, tag) {
+			//a more civilized weapon for deleting to the left
+			function rightMost(parent, tag) {
+				if (tag.right.length === 0 && tag.left.length === 0) {
+					return tag;
+				} else if (tag.left.length !== 0 && tag.right.length === 0) {
+					return rightMost(tag, tag.left[0]);
+				} else {
+					return rightMost(tag, tag.right[0]);
+				}
+			}
+
+			if (tag.left.length === 0 && tag.right.length === 0) {//remove left
+				parent.left = [];
+			} else if (tag.left.length === 0 && tag.right.length !== 0) {//place right in parent left
+				parent.left = tag.right;
+			} else if (tag.left.length !== 0 && tag.right.length === 0) {//place left in parent left
+				parent.left = tag.left;
+			} else {//make right most
+				parent.left.name = rightMost(tag, tag.left[0]).name;
+			}
+		}
+
+		//a more civilized weapon for deleting to the right
+		function deleteRight(parent, tag) {
+			function leftMost(parent, tag) {
+				if (tag.right.length === 0 && tag.left.length === 0) {
+					return tag;
+				} else if (tag.left.length === 0 && tag.right.length !== 0) {
+					return leftMost(tag, tag.right[0]);
+				} else {
+					return leftMost(tag, tag.left[0]);
+				}
+			}
+
+			if (tag.left.length === 0 && tag.right.length === 0) {//remove left
+				parent.right = [];
+			} else if (tag.left.length === 0 && tag.right.length !== 0) {//place right in parent left
+				parent.right = tag.right;
+			} else if (tag.left.length !== 0 && tag.right.length === 0) {//place left in parent left
+				parent.right = tag.left;
+			} else {//make right most
+				parent.right.name = leftMost(tag, tag.right[0]).name;
+			}
+		}
+
+		try {
+			console.log('parent:'+parent);
+			console.log('tag:'+tag);
+			if (parent.left[0] === tag) {//deleting a left
+					parent.left = [];
+			} else if (parent.right[0] === tag) {//deleting a right
+					parent.right = [];
+			}
+		} catch (e) {
+			console.log(e);
+			alert('this node cannot be unlinked');
+		}
 	}
 }]);
